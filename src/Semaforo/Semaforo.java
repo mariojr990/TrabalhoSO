9b4cea7dc376e07cf792746d2fe28702d95147f3
mario cesar dos santos junior <36646986+mariojr990@users.noreply.github.com>
2019-06-11 10:00:35 -0300

    Update and rename condiao_corrida_semaforo.java to Semaforo.java

--- src/Semaforo/Semaforo.java
+++ src/Semaforo/Semaforo.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Semaforo;
 
-/**
- *
- * @author mariojr990
- */
 import java.util.concurrent.Semaphore;
 
 public class condiao_corrida_semaforo extends Thread {
